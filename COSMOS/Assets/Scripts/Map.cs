5c58a885da7176ebbcbd6d2dcec462fafeb0e2fe
mmeo009 <shh020615@gmail.com>
2023-06-26 13:11:09 +0900

    맵 생성 및 이동 히히

---
+++ COSMOS/Assets/Scripts/Map.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject mapSystem;
+    public int level;
+    float speed;
+    private void Awake()
+    {
+        mapSystem = GameObject.FindGameObjectWithTag("MapGen").gameObject;
+    }
+    private void FixedUpdate()
+    {
+        speed = mapSystem.GetComponent<MapGenerator>().speed;
+        this.transform.Translate(Vector3.right * speed);
+    }
+
+    private void Update()
+    {
+        if(this.transform.position.x <= -20 - speed)
+        {
+            mapSystem.GetComponent<MapGenerator>().MapGen(level);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ COSMOS/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject[] level_1;
+    public float speed = 0.01f;
+    private void FixedUpdate()
+    {
+        speed += 0.001f * Time.fixedDeltaTime;
+    }
+
+    public void MapGen(int level)
+    {
+        Debug.Log("¸¸µç´Ù?");
+        switch(level)
+        {
+            case 1:
+                int Lv1num = Random.Range(0, level_1.Length);
+                GameObject temp = (GameObject)Instantiate(level_1[Lv1num]);
+                temp.transform.position = new Vector2 (20,0);
+                Debug.Log("1·¦ ¸¸µë È÷È÷");
+                break;
+        }
+
+    }
+}

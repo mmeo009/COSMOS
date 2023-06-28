2eabc04786c4bda1d34bffba016069c556653122
flexinja99 <hjgo927@naver.com>
2023-06-28 09:09:06 +0900

    오디오

---
+++ COSMOS/Assets/Scripts/MC.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class MC : MonoBehaviour
+{
+    public string name;
+
+    public AudioClip clip;
+    public AudioMixerGroup mixer;
+
+    [Range(0f, 1f)]
+    public float volume = 1;
+
+    [Range(-3f, 3f)]
+    public float pitch = 1;
+
+    public bool loop = false;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
